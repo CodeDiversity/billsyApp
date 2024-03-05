e783e675b5f8fd2b368665c6533d0278289fec37
Michael Trew <mtrew2015@gmail.com>
2024-03-05 10:39:00 -0500

    fixing protected route

--- src/features/Authentication/hooks/protectedRoute.tsx
+++ src/features/Authentication/hooks/protectedRoute.tsx
@@ -1,18 +1,15 @@
 // RequireAuth.tsx
 import React, { useEffect } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
-import { useAuth } from "./useAuth";
 
 export const RequireAuth = () => {
-  const isAuthenticated = useAuth();
-  console.log(isAuthenticated);
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (!isAuthenticated) {
+    const token = localStorage.getItem("token");
+    if (!token) {
       navigate("/");
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [navigate]);
   return <Outlet />;
 };

0e018da68ffb1402d9748a8000589ebd9bbae86f
Michael Trew <mtrew2015@gmail.com>
2024-03-11 23:11:41 -0400

    settings

--- src/features/Authentication/thunks/userThunks.ts
+++ src/features/Authentication/thunks/userThunks.ts
@@ -1,9 +1,9 @@
 // src/features/user/userThunks.ts
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { logoutUser, setError, setToken, setUser } from '../slices/userSlice';
+import { logoutUser, setError, setToken, setUser, updateUserInfo } from '../slices/userSlice';
 import { getErrorMessage } from '../../../common/errorMessages';
 import client from '../../../axiosConfig';
-import { ErrorResponse, LoginResponse, LoginValues, User } from '../types/userTypes';
+import { ErrorResponse, LoginResponse, LoginValues, SettingsUser, User } from '../types/userTypes';
 import { RootState } from '../../../store';
 import { fetchBills } from '../../Bills/thunks/billThunks';
 
@@ -69,6 +69,28 @@ export const rehydrateAuthState = createAsyncThunk<
   dispatch(logoutUser());
   return null;
 });
+
+export const updateUser = createAsyncThunk(
+  'user/updateUser',
+  async (values: SettingsUser, { dispatch, rejectWithValue }) => {
+    try {
+      const response = await client.patch<SettingsUser>("users", values);
+      dispatch(updateUserInfo(response.data));
+    } catch (error) {
+      const err = error as ErrorResponse;
+      let errorMessage = "An unexpected error occurred. Please try again.";
+
+      if (err.response?.data?.message) {
+        errorMessage = err.response.data.message;
+      } else if (err.response?.data?.code) {
+        errorMessage = getErrorMessage(err.response.data.code);
+      }
+
+      dispatch(setError(errorMessage));
+      return rejectWithValue(errorMessage);
+    }
+  }
+);
 
 async function fetchUserData(): Promise<User> {
   const response = await client.get<User>('users');
--- src/features/Settings/components/Settings.tsx
+++ src/features/Settings/components/Settings.tsx
@@ -1,8 +1,159 @@
 import React from "react";
-import { LeftNav } from "../../Dashboard/components/LeftNav/LeftNav";
-import { NavBar } from "../../../common/NavBar/NavBar";
 import { LoggedInLayout } from "../../../common/Layouts/LoggedInLayout";
+import { useFormik } from "formik";
+import { useDispatch, useSelector } from "react-redux";
+import { selectCurrentUser, updateUserInfo } from "../../Authentication/slices/userSlice";
+import { MenuItem, Select, TextField } from "@mui/material";
+import styled from "@emotion/styled";
+import { updateUser } from "../../Authentication/thunks/userThunks";
+import { AppDispatch } from "../../../store";
+import { SettingsUser, User } from "../../Authentication/types/userTypes";
+import { toast } from "react-toastify";
 
 export default function Settings() {
-  return <LoggedInLayout>Settings</LoggedInLayout>;
+  const user = useSelector(selectCurrentUser) as SettingsUser
+  const dispatch = useDispatch<AppDispatch>();
+  console.log(user);
+  const formik = useFormik({
+    initialValues: {
+      email: user?.email || "",
+      fullName: user?.fullName || "",
+      phoneNumber: user?.phoneNumber || "",
+      emailReminderTime: user?.emailReminderTime || "",
+      currency: user?.currency || "",
+    },
+    onSubmit: async (values: SettingsUser) => {
+      const user = {
+        ...values,
+      };
+      await dispatch(updateUser(user)).unwrap();
+      toast.success("User settings updated!");
+    },
+    enableReinitialize: true,
+  });
+
+  return (
+    <LoggedInLayout>
+      <Header>Settings</Header>
+      <Form onSubmit={formik.handleSubmit}>
+        <InputSection>
+          <label htmlFor="email">Email</label>
+          <TextField
+            id="email"
+            name="email"
+            type="email"
+            onChange={formik.handleChange}
+            value={formik.values.email}
+          />
+        </InputSection>
+        <InputSection>
+          <label htmlFor="name">Name</label>
+          <TextField
+            id="name"
+            name="name"
+            type="text"
+            onChange={formik.handleChange}
+            value={formik.values.fullName}
+          />
+        </InputSection>
+        <InputSection>
+          <label htmlFor="phoneNumber">Phone Number</label>
+          <TextField
+            id="phoneNumber"
+            name="phoneNumber"
+            type="text"
+            onChange={formik.handleChange}
+            value={formik.values.phoneNumber}
+          />
+        </InputSection>
+        <InputSection>
+          {/* make the email time a dropdown */}
+          <label htmlFor="emailReminderTime">Email Reminder Time</label>
+          <Select
+            id="emailReminderTime"
+            name="emailReminderTime"
+            displayEmpty
+            onChange={formik.handleChange}
+            value={formik.values.emailReminderTime}
+            sx={{ height: 30, width: 100 }}
+          >
+            {/* options 12a to 12p */}
+            {Array.from({ length: 24 }, (_, i) => i).map((hour) => (
+              <MenuItem key={hour} value={hour}>
+                {hour % 12 === 0 ? 12 : hour % 12} {hour < 12 ? "a" : "p"}
+              </MenuItem>
+            ))}
+          </Select>
+        </InputSection>
+        <InputSection>
+          <label htmlFor="currency">Currency</label>
+          <TextField
+            id="currency"
+            name="currency"
+            type="text"
+            onChange={formik.handleChange}
+            value={formik.values.currency}
+          />
+        </InputSection>
+        <SubmitButton type="submit">Submit</SubmitButton>
+      </Form>
+    </LoggedInLayout>
+  );
 }
+
+const Wrapper = styled.section`
+  display: flex;
+  height: 100vh;
+  flex-direction: column;
+  align-items: flex-start;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  width: 40%;
+  padding: 2rem;
+  border-radius: 4px;
+`;
+
+const Header = styled.h1`
+  font-size: 2.5rem;
+  margin-bottom: 1rem;
+  margin-left: 2%;
+`;
+
+const InputSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+`;
+
+const Input = styled.input`
+  padding: 16px;
+  border-radius: 0.5rem;
+  border: 1px solid #ddd;
+  width: 93%;
+  margin-left: 5px;
+  margin-top: 10px;
+`;
+
+const SubmitButton = styled.button`
+  padding: 0.5rem;
+  border-radius: 4px;
+  border: none;
+  background-color: #007bff;
+  color: white;
+  font-size: 1rem;
+  cursor: pointer;
+  transition: background-color 0.2s;
+  &:hover {
+    background-color: #0056b3;
+  }
+`;
+
+const StyledError = styled.div`
+  color: red;
+  font-size: 0.875rem;
+  font-weight: 800;
+`;

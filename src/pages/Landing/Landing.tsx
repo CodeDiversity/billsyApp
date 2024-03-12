e6a928c9a33038e3c6761b360797345cbc50a3d0
Michael Trew <mtrew2015@gmail.com>
2024-03-11 21:56:13 -0400

    clean up styles a bit

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "@emotion/styled";
-import { NavBar } from "../../common/NavBar/NavBar";
 import { Dashboard } from "../../features/Dashboard/components/Dashboard";
 import { MarketingPage } from "../MarketingPage/MarketingPage";
 import { useIsUserLoggedIn } from "../../features/Authentication/hooks/useIsUserLoggedIn";

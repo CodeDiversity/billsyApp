2f0c6a0682953b7c672c0a8f4b639bc1b8d9b78a
Michael Trew <mtrew2015@gmail.com>
2024-03-13 10:49:47 -0400

    fix tests

--- src/common/NavBar/components/NavBar.test.tsx
+++ src/common/NavBar/components/NavBar.test.tsx
@@ -1,8 +1,15 @@
 import { NavBar } from "./NavBar";
 import { renderWithProviders } from "../../../utils/test-utils";
+import React from "react";
+import { screen } from "@testing-library/react";
 
 describe("NavBar", () => {
   it('should render without error', () => {
     renderWithProviders(<NavBar />);
+  })
+  it('should render the logo', () => {
+    renderWithProviders(<NavBar />);
+    const logo = screen.getByText(/billsy/i);
+    expect(logo).toBeInTheDocument();
   })
 });

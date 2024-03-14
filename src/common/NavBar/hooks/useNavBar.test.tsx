d44fad7207963b4a4e68effb4e25cbcf26899e0f
Michael Trew <mtrew2015@gmail.com>
2024-03-13 21:53:31 -0400

    adding one more test for useNavbar

--- src/common/NavBar/hooks/useNavBar.test.tsx
+++ src/common/NavBar/hooks/useNavBar.test.tsx
@@ -1,8 +1,6 @@
 import { useNavBar } from "./useNavBar";
 import * as router from "react-router";
-import {
-  renderHookWithProviders,
-} from "../../../utils/test-utils";
+import { renderHookWithProviders } from "../../../utils/test-utils";
 
 describe("NavBar Hook - Navigation Tests", () => {
   const nav = jest.fn();
@@ -18,11 +16,18 @@ describe("NavBar Hook - Navigation Tests", () => {
     localStorage.clear();
   });
 
-  it("should return false when token is expired", () => {
+  it("should return true when token is expired", () => {
     const { result } = renderHookWithProviders(() => useNavBar());
     const { isTokenExpired } = result.current;
     let token =
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MTAyOTM4NjUsInN1YiI6InVzZXJfaWRfMTIzNDUifQ.1Rl3S3pBvMppTsNSasPzHS5VFpGhYFtjH8qBcSpHWu0";
     expect(isTokenExpired(token)).toBe(true);
+  });
+  it("should return false when token is not expired", () => {
+    const { result } = renderHookWithProviders(() => useNavBar());
+    const { isTokenExpired } = result.current;
+    let token =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE4NjgwNjExMjYsInN1YiI6InVzZXJfaWRfMTIzNDUifQ.xlwGX6-fEMovQmp-Mzl2hqCwcnpGmSYqfthc3w2vnwc";
+    expect(isTokenExpired(token)).toBe(false);
   });
 });

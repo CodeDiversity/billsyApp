64060d12807034739e93d8125d5de482bf871b57
Michael Trew <mtrew2015@gmail.com>
2024-04-27 09:37:11 -0400

    clean up thunk

--- src/features/Payments/thunks/paymentThunks.ts
+++ src/features/Payments/thunks/paymentThunks.ts
@@ -2,16 +2,17 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import client from "../../../axios/axiosConfig";
 import { Payment } from "../types";
 import { fetchBills } from "../../Bills/thunks/billThunks";
+import { AxiosError } from "axios";
 
 export const createPayment = createAsyncThunk(
   "payments/createPayment",
-  async (payment: Payment, { dispatch }) => {
+  async (payment: Payment, { dispatch, rejectWithValue }) => {
     try {
       const response = await client.post("/payment", payment);
       dispatch(fetchBills());
       return response.data;
-    } catch (error) {
-      // return rejectWithValue(error.response.data);
+    } catch (error: any) {
+      return rejectWithValue((error as AxiosError).response?.data);
     }
   }
 );

0a2729b7f547006eb642b937f8dd03cb3a3b2331
Michael Trew <mtrew2015@gmail.com>
2024-05-12 23:41:55 -0400

    help page

--- src/features/Help/components/Help.tsx
+++ src/features/Help/components/Help.tsx
@@ -2,5 +2,17 @@ import React from "react";
 import { LoggedInLayout } from "../../../common/Layouts/LoggedInLayout";
 
 export default function Help() {
-  return <LoggedInLayout>Help</LoggedInLayout>;
+  return (
+    <LoggedInLayout>
+      <h1>Help</h1>
+      <p>
+        This is a help page.
+        It is not implemented yet.
+        Please help us out by contributing to the project on{" "}
+        <a href="https://github.com/CodeDiversity/billsyApp">GitHub</a>.
+      </p>
+    </LoggedInLayout>
+  )
 }
+
+

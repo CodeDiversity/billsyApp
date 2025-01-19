40af7bef95425076d8ef12a362c0b1a686528744
Michael Trew <40902786+CodeDiversity@users.noreply.github.com>
2025-01-19 16:53:35 -0500

    rm unused comments

--- src/features/Bills/components/CreateBillForm/CreateBill.tsx
+++ src/features/Bills/components/CreateBillForm/CreateBill.tsx
@@ -316,20 +316,3 @@ const StyledError = styled.div`
   font-size: 0.875rem;
   font-weight: 800;
 `;
-
-interface FormErrors {}
-
-/*
-
- userId: { type: ObjectId, required: true, ref: 'User' },
-  name: { type: String, required: true },
-  amount: { type: Number, required: true },
-  dueDate: { type: Date },
-  isPaid: { type: Boolean, required: true, default: false },
-  created: { type: Date, default: Date.now },
-  isRecurring: { type: Boolean, required: true, default: false },
-  category: { type: String },
-  createdAt: { type: Date, default: Date.now },
-  lastPaidAt: { type: Date },
-
-*/
